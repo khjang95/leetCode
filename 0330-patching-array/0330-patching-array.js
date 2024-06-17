9aa556bd902c7e501100c4a3d4b8328cd5961aea
Jang Kwang Hyun <83074152+khjang95@users.noreply.github.com>
2024-06-17 20:34:52 +0900

    Time: 52 ms (67.39%), Space: 49.1 MB (58.7%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+var minPatches = function(nums, n) {
+    let missing = 1;
+    let patches = 0;
+    let index = 0;
+
+    while (missing <= n) {
+        if (index < nums.length && nums[index] <= missing) {
+            missing += nums[index];
+            index++;
+        } else {
+            missing += missing;
+            patches++;
+        }
+    }
+
+    return patches;
+};
