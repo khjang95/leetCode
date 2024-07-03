8c59e93bff201971202498a5eefcf14410419579
Jang Kwang Hyun <83074152+khjang95@users.noreply.github.com>
2024-07-03 21:31:35 +0900

    Time: 64 ms (55.77%), Space: 56 MB (16.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,23 +2,33 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
 
-    let left = 0;
+ function isAlphanumeric(char) {
+    return /^[a-z0-9]$/i.test(char);
+}
+
+var isPalindrome = function(s) {
+        let left = 0;
     let right = s.length - 1;
 
-    if (s.length === 0) {
-        return true;
-    }
-
     while (left < right) {
-        if (s[left] === s[right]) {
-            left ++;
-            right --;
-        } else {
+        // 왼쪽 포인터가 알파벳이나 숫자가 아닐 경우 건너뜁니다.
+        while (left < right && !isAlphanumeric(s[left])) {
+            left++;
+        }
+        // 오른쪽 포인터가 알파벳이나 숫자가 아닐 경우 건너뜁니다.
+        while (left < right && !isAlphanumeric(s[right])) {
+            right--;
+        }
+
+        // 현재 위치의 문자를 소문자로 변환하여 비교합니다.
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
             return false;
         }
+
+        // 포인터를 이동시킵니다.
+        left++;
+        right--;
     }
 
     return true;
