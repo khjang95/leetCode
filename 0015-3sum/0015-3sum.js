5fd796157ce6cdc3342742fbbe26c3432da7923d
Jang Kwang Hyun <83074152+khjang95@users.noreply.github.com>
2024-07-11 21:11:19 +0900

    Time: 262 ms (18.05%), Space: 67.3 MB (11.67%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -2,14 +2,17 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
+var threeSum = function (nums) {
     let results = [];
-    if (nums.length < 3) return results;
 
     nums.sort((a, b) => a - b);
 
+console.log(nums)
+
     for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue; // 중복된 값 건너뛰기
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        } 
 
         let left = i + 1;
         let right = nums.length - 1;
@@ -19,8 +22,16 @@ var threeSum = function(nums) {
 
             if (sum === 0) {
                 results.push([nums[i], nums[left], nums[right]]);
-                while (nums[left] === nums[left + 1]) left++; // 중복된 값 건너뛰기
-                while (nums[right] === nums[right - 1]) right--; // 중복된 값 건너뛰기
+
+                while (nums[left] === nums[left + 1]) {
+                    console.log(1)
+                    left++;
+                }
+
+                while (nums[right] === nums[right - 1]) {
+                    right--;
+                }
+
                 left++;
                 right--;
             } else if (sum < 0) {
