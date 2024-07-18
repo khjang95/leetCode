5287efd12c5ff763699526be8db2cae5c2ddcd3c
Jang Kwang Hyun <83074152+khjang95@users.noreply.github.com>
2024-07-18 22:09:25 +0900

    Time: 75 ms (5.24%), Space: 51.7 MB (6.79%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -9,20 +9,22 @@ var maxProfit = function (prices) {
     let greedy = 0;
 
     while (greedy < pricesLength - 1) {
+        if (prices[greedy] > prices[greedy + 1]) {
+            greedy++;
+            continue;
+        }
 
         for (let i = greedy + 1; i < pricesLength; i++) {
-            if (prices[greedy] > prices[i]) {
-                greedy++;
-                break;
-            }
-
             if (prices[i] > prices[i + 1]) {
+                console.log(i)
+                console.log(greedy)
                 result += prices[i] - prices[greedy];
+
                 greedy += i - greedy;
+                break;
             } else if (i === pricesLength - 1) {
                 return result + prices[i] - prices[greedy];
             }
-
         }
     }
 
