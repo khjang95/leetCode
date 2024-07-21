a4a93f9662a9547c2deb4a22eabfd13f8f171ad9
Jang Kwang Hyun <83074152+khjang95@users.noreply.github.com>
2024-07-21 22:00:02 +0900

    Time: 51 ms (86.51%), Space: 51.1 MB (56.18%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -7,9 +7,9 @@ var jump = function(nums) {
         return 0;
     }
 
-    let jump = 0;
     let currentEnd = 0;
     let maxJump = 0;
+    let jump = 0;
 
     for (let i = 0; i < nums.length - 1; i++) {
         maxJump = Math.max(maxJump, i + nums[i]);
