463a0ffde6021364f013ab0bfe19292e2f203e44
Jang Kwang Hyun <83074152+khjang95@users.noreply.github.com>
2024-08-06 20:51:04 +0900

    Time: 193 ms (5.61%), Space: 58.8 MB (5.33%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const symbol = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    let index = 0;    
+    let result = 0;
+
+    while (index < s.length) {
+        const sum = symbol[s[index + 1]] - symbol[s[index]];
+
+        if (index === s.length - 1) {
+            return result + symbol[s[index]];
+        } 
+        
+        if (sum > 0 && sum * 10 % 10 === 0) {
+            result += sum;
+            index += 2;
+        } else {
+            result += symbol[s[index]];
+            index += 1;
+        }
+        
+        console.log(index)
+        // 4, 9, 40, 90, 400, 900
+    }
+
+    return result;
+};
