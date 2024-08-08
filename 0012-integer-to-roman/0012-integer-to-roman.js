948f4bf5d8d7223f2c78605e29e01c1debdba7ab
Jang Kwang Hyun <83074152+khjang95@users.noreply.github.com>
2024-08-08 21:55:21 +0900

    Time: 108 ms (76.33%), Space: 59.1 MB (5.37%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -17,13 +17,13 @@ var intToRoman = function (num) {
 
 var transRoman = function (number, digit) {
     var roman = {
-        '1': 'I',
-        '5': 'V',
-        '10': 'X',
-        '50': 'L',
-        '100': 'C',
-        '500': 'D',
-        '1000': 'M'
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M'
     }
     var standard = 5;
     var digits = digit === 0 ? 1 : 10 ** digit;
